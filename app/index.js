79f9024a0e6358e92a0df0f1e93817d343ac303c
Josh Hodgson <josh.pbh@gmail.com>
2016-02-16 14:57:45 +0000

    Initial web server running

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
-console.log("Hello!");
+var express = require('express');
+var socket = require('socket.io');
+
+//Start web server
+var app = express();
+var server = app.listen(8080);
+var io = socket.listen(server);
+
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+  var data = {connected:true, markers:[{timecode:'1234'},{timecode:'123456'}]
+  };
+  res.render('pages/index.ejs', data);
+});
