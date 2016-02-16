60233a79f397df4ecbc376f9d9009adb852afbf5
Josh Hodgson <josh.pbh@gmail.com>
2016-02-16 16:42:58 +0000

    Added hyperdeck.js as module

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var socket = require('socket.io');
-var hyperdeck = require('./hyperdeck.js')
+var hyperdeck = require('./hyperdeck.js');
 
 //Start web server
 var app = express();
@@ -9,8 +9,8 @@ var io = socket.listen(server);
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('stop', function(){console.log('stop'); hyperdeck.stop()});
-  socket.on('play', function(payload){console.log('play ' + payload); hyperdeck.play(payload)});
+  socket.on('stop', function(){console.log('stop'); hyperdeck.stop();});
+  socket.on('play', function(payload){console.log('play ' + payload); hyperdeck.play(payload);});
   socket.on('goto', function(payload){console.log('goto ' + payload);});
 });
 
