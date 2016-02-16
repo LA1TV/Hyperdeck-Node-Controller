07f70354a658fab54a5418d5d7b1673838f38328
Lagit01 <ben.kay01@hotmail.co.uk>
2016-02-16 16:27:14 +0000

    just one play function
    if no time %age input then it outputs at 100%

--- app/hyperdeck.js
+++ app/hyperdeck.js
@@ -24,24 +24,22 @@ client.connect({host: '192.168.72.64', port: 9993}, function() {
 //Set timecode
 
 //play at standard speed
-function play(){
-	try{
-		client.write('play\n');
-		console.log('Playing');
-	}catch (err){
-		console.log(err);
-	}
-}
 
-//play at a predefined speed
-function playAtSpeed(speed){
+
+//play at a predefined speed or standard speed
+function play(speed){
 	try{
-		var play = "play: speed: ";
-		speed = speed.toString();
-		var playSpeed = play.concat(speed);
-		client.write(playSpeed + '\n');
-		console.out
-		console.log('Playing at speed: ' + speed);
+		if(!speed){
+			client.write('play\n');
+			console.log('Playing');
+		}else{
+			var play = "play: speed: ";
+			speed = speed.toString();
+			var playSpeed = play.concat(speed);
+			client.write(playSpeed + '\n');
+			console.out
+			console.log('Playing at speed: ' + speed);
+		}
 	}catch (err){
 		console.log(err);
 	}
@@ -51,13 +49,13 @@ function playAtSpeed(speed){
 function stop(){
 	try{
 		client.write('stop\n');
-		console.log('Playing');
+		console.log('Stoping');
 	}catch (err){
 		console.log(err);
 	}
 }
 
-setTimeout(function(){playAtSpeed(40)},5000);
+setTimeout(function(){play(20)},1000);
 
 
 
