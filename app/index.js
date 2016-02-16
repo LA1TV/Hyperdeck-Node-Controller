3257ec2b01d4eaf1fe92c6cf3de9ea1da93d209d
Josh Hodgson <josh.pbh@gmail.com>
2016-02-16 16:26:37 +0000

    Enable sockets

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var socket = require('socket.io');
+var hyperdeck = require('./hyperdeck.js')
 
 //Start web server
 var app = express();
@@ -7,7 +8,13 @@ var server = app.listen(8080);
 var io = socket.listen(server);
 
 io.on('connection', function(socket){
-  console.log('a user connected');});
+  console.log('a user connected');
+  socket.on('stop', function(){console.log('stop'); hyperdeck.stop()});
+  socket.on('play', function(payload){console.log('play ' + payload); hyperdeck.play(payload)});
+  socket.on('goto', function(payload){console.log('goto ' + payload);});
+});
+
+
 
 
 app.set('view engine', 'ejs');
@@ -16,3 +23,5 @@ app.get('/', function(req, res) {
   };
   res.render('pages/index.ejs', data);
 });
+
+hyperdeck.play(100);
