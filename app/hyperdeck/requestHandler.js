3638b507f44dc3fec398f340b8b5d3b54ad32fdf
joshhodgson <josh.pbh@gmail.com>
2016-03-18 18:45:40 +0000

    JSHint Error Fixing

--- app/hyperdeck/requestHandler.js
+++ app/hyperdeck/requestHandler.js
@@ -1,9 +1,10 @@
 var parser = require('./parser.js');
+var Promise = require('promise');
+
 
 var pendingRequests = [];
 var requestCompletionPromise = [];
 var requestInProgress = false;
-var hyperdeckClient = "";
 
 /**
  * The Request handler for the Hyperdeck API.
@@ -11,6 +12,44 @@ var hyperdeckClient = "";
  * @param client, The client connection to the hyperdeck.
  **/
 function RequestHandler(client) {
+
+  /**
+   * Checks the chain isn't empty or that the request is in progress.
+   * Then takes from the bottom of the chain and do the request.
+   * Once the request has finished do more things.
+   * Performs the next request based of the chain, runs until the chain is empty.
+   **/
+  function performNextRequest() {
+    if (pendingRequests.length === 0 || requestInProgress) {
+      return; //returns if theres nothing left in the chain or theres a request in progress.
+    }
+
+    requestInProgress = true;
+    var request = pendingRequests.shift();
+
+    client.write(request);
+
+    // Listen for a response, either error, or data.
+    function handleResponse(response) {
+      requestCompletionPromise.shift().resolve(response);
+    }
+
+    function handleResponseClientError(response) {
+      requestCompletionPromise.shift().reject(response);
+    }
+
+    function handleError(response) {
+      requestCompletionPromise.shift().reject(response);
+    }
+    parser.getNotifier().one("synchronousEvent", function syncEventListenetr(response){handleResponse(response);});
+    parser.getNotifier().one("synchronousEventError", function(response){handleResponseClientError(response);});
+    parser.getNotifier().one("error", function errorListener(response){handleError(response);});
+
+
+
+    requestInProgress = false;
+    performNextRequest();
+  }
 
   // Response Listener.
   try{
@@ -36,48 +75,7 @@ function RequestHandler(client) {
     return completionPromise;
   };
 
-  /**
-   * Checks the chain isn't empty or that the request is in progress.
-   * Then takes from the bottom of the chain and do the request.
-   * Once the request has finished do more things.
-   * Performs the next request based of the chain, runs until the chain is empty.
-   **/
-  function performNextRequest() {
-    if (pendingRequests.length === 0 || requestInProgress) {
-      return; //returns if theres nothing left in the chain or theres a request in progress.
-    }
 
-    requestInProgress = true;
-    var request = pendingRequests.shift();
-
-    client.write(request);
-
-    // Listen for a response, either error, or data.
-	  parser.getNotifier().one("synchronousEvent", function(response){handleResponse(response);});
-    parser.getNotifier().one("synchronousEventError", function(response){handleResponseClientError(response);});
-    parser.getNotifier().one("error", function(response){handleError(error);});
-
-    function handleResponse(response) {
-      parser.getNotifer().off("synchronousEventError", data);
-      parser.getNotifer().off("error", data);
-      requestCompletionPromise.shift().resolve(data);
-    }
-
-    function handleResponseClientError(response) {
-      parser.getNotifer().off("synchronousEvent", data);
-      parser.getNotifer().off("error", data);
-      requestCompletionPromise.shift().reject(data);
-    }
-
-    function handleError(response) {
-      parser.getNotifer().off("synchronousEvent", data);
-      parser.getNotifer().off("synchronousEventError", data);
-      requestCompletionPromise.shift().reject(data);
-    }
-
-    requestInProgress = false;
-    performNextRequest();
-  }
 
   // /**
   //  * Performs the request made by the Hyperdeck
