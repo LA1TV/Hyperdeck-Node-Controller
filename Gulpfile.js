f6bc160b42621305a181edfcc483e2b0d55b67e9
Tom Jenkinson <tjcode@outlook.com>
2016-03-22 16:39:11 +0000

    Started parser unit tests

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var jshint = require('gulp-jshint');
-var gulp   = require('gulp');
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 
 gulp.task('lint', function() {
   return gulp.src(['src/*.js', 'src/**/*.js'])
@@ -8,4 +9,15 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('default', ['lint']);
+gulp.task('test', function() {
+  return gulp.src(['test/*.js', 'test/**/*.js'], {read: false})
+    // gulp-mocha needs filepaths so you can't have any plugins before it 
+    .pipe(mocha({
+        reporter: 'spec',
+        globals: {
+            should: require('should')
+        }
+    }));
+});
+
+gulp.task('default', ['lint', 'test']);
---
+++ test/hyperdeck/parser.js
@@ -0,0 +1,73 @@
+var Parser = require('../../src/hyperdeck/parser');
+
+var SUCCESS_RESPONSE = "200 Success";
+var SUCCESS_RESPONSE_WITH_PARAMS = "200 Sucess with data:\r\nsomething: 123\r\nsomething else: test\r\n\r\n";
+var FAILURE_RESPONSE = "102 Failure";
+var FAILURE_RESPONSE_WITH_PARAMS = "102 Failure:\r\nsomething: 123\r\nsomething else: test\r\n\r\n";
+var ASYNC_RESPONSE = "512 Async event:\r\nprotocol version: 9.5\r\nmodel: xyz\r\ntime: 12:40:12\r\n\r\n";
+
+var SUCCESS_RESPONSE_DATA = {
+    type: "synchronousSuccess",
+    data: {
+        title: "200 Success"
+    }
+};
+
+var SUCCESS_PARAMS_RESPONSE_DATA = {
+    type: "synchronousSuccess",
+    data: {
+        title: "200 Success with data",
+        something: "123",
+        "something else": "test"
+    }
+};
+
+var FAILURE_RESPONSE_DATA = {
+    type: "synchronousFailure",
+    data: {
+        title: "102 Failure"
+    }
+};
+
+var FAILURE_PARAMS_RESPONSE_DATA = {
+    type: "synchronousFailure",
+    data: {
+        title: "102 Failure",
+        something: "123",
+        "something else": "test"
+    }
+};
+
+var ASYNC_RESPONSE_DATA = {
+    type: "asynchronous",
+    data: {
+        title: "512 Async event",
+        "protocol version": "9.5",
+        model: "xyz",
+        time: "12:40:12"
+    }
+};
+
+describe('Parser', function() {
+
+  it('should handle a response string with a success status code', function() {
+    Parser.parse(SUCCESS_RESPONSE).should.eql(SUCCESS_RESPONSE_DATA);
+  });
+
+  it('should handle a response string with a success status code and params', function() {
+    Parser.parse(SUCCESS_RESPONSE_WITH_PARAMS).should.eql(SUCCESS_PARAMS_RESPONSE_DATA);
+  });
+
+  it('should handle a response string with a failure status code', function() {
+    Parser.parse(FAILURE_RESPONSE).should.eql(FAILURE_RESPONSE_DATA);
+  });
+
+  it('should handle a response string with a failure status code and params', function() {
+    Parser.parse(FAILURE_RESPONSE_WITH_PARAMS).should.eql(FAILURE_PARAMS_RESPONSE_DATA);
+  });
+
+  it('should handle a response string with an async status code', function() {
+    Parser.parse(ASYNC_RESPONSE).should.eql(ASYNC_RESPONSE_DATA);
+  });
+
+});
