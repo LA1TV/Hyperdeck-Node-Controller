80280c1334f293a245c0afd2215085fcdd037c5d
Benjamin Kay <ben.kay01@hotmail.co.uk>
2017-10-18 20:06:18 +0100

    Let the linting commence

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,11 @@
-var jshint = require('gulp-jshint');
-var gulp   = require('gulp');
+var jshint = require('gulp-jshint')
+var gulp = require('gulp')
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src(['*.js', '**/*.js', '!node_modules/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
-});
+    .pipe(jshint.reporter('fail'))
+})
 
-gulp.task('default', ['lint']);
+gulp.task('default', ['lint'])
